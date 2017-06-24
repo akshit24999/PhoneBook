c62d207de815943678c621a1ff9c8236f7f8f4a9
santosh <kv.santosh@gmail.com>
2017-06-24 21:38:51 +0530

    saving&reading from file

--- phone_book.c
+++ phone_book.c
@@ -13,6 +13,8 @@
  int search(char * name,struct phonebook abc[]);
  int find_empty_index(struct phonebook abc[]);
  int print_contacts(struct phonebook abc[]);
+ int save_contact_to_file(struct phonebook abc[],int find_empty_index);
+ int read_from_file(struct phonebook p[]);
  int main()
 {
     int user_choice,k,phone;
@@ -20,13 +22,13 @@
     struct phonebook p[10];
     int search_index,index_searched, for_index = 0, last_index =0,new_phone;
     clrscr();
-
     //initializing p[]
     for(k=0;k<10;k++)
     {
      p[k].name[0]=0;
      p[k].email[0]=0;
     }
+    read_from_file(p);
     while(1)
     {
 	printf("enter anyone of the following options \n 1:Add a contact\n 2:Edit a cotact\n 3:Delete a contact\n 4:exit\n 5.Print Contacts\n");
@@ -42,9 +44,11 @@
 		   index_searched=find_empty_index(p);
 		   printf("\nAdding contact to index : %d\n",index_searched);
 		   add_contact(&p[index_searched],name,email,phone);
+		   save_contact_to_file(p,index_searched);
 		       break;
 	    case 2:printf("enter the contact to be edited \n");
 		       scanf("%s",name);
+		       index_searched=find_empty_index(p);
 		       search_index = search(name,p);
 		       printf("contact found at: %d \n",search_index);
 		       printf("enter the new name \n");
@@ -54,23 +58,28 @@
 		       printf("enter the new phone \n");
 		       scanf("%d",&new_phone);
 		       edit(&p[search_index],new_name,new_email,new_phone);
+		       save_contact_to_file(p,index_searched);
 		       break;
 	    case 3:printf("enter the contact to be deleted \n");
 		       scanf("%s",name2);
 		       search_index = search(name2,p);
 		       index_searched=find_empty_index(p);
 		       del(search_index,index_searched,p);
+		       index_searched=find_empty_index(p);
+		       save_contact_to_file(p,index_searched);
 		       break;
 	    case 4:
 		       break;
 		case 5:
 			 last_index = find_empty_index(p);
-		      for(for_index = 0; for_index < last_index ; for_index++){
+		      for(for_index = 0; for_index < last_index ; for_index++)
+		      {
 			printf("Contact %d : \n", for_index);
 			printf ("Name : %s\n", p[for_index].name);
 			printf ("Email : %s\n", p[for_index].email);
 			printf ("Phone Number : %d\n", p[for_index].ph_no);
 		      }
+		      save_contact_to_file(p,last_index);
 		      break;
 	    default: printf("enter any valid option");
 		    break;
@@ -144,5 +153,55 @@ int find_empty_index(struct phonebook p[])
 
     return -1;
 }
-
-
+int save_contact_to_file(struct phonebook p[],int find_empty_index)
+{
+ FILE *contacts;
+ int a;
+ char phone[10],concat[100];
+ concat[0] = 0;
+ contacts=fopen("c://tc//bin//phone.txt","w+");
+ for(a=0;a< find_empty_index;a++)
+ {
+   itoa(p[a].ph_no,phone,10);
+   strcat(concat,p[a].name);
+   strcat(concat,",");
+   strcat(concat,p[a].email);
+   strcat(concat,",");
+   strcat(concat,phone);
+   strcat(concat,"\n");
+  // concat = strcat(p[a].name,",",p[a].email,",",phone,"\n");
+   fputs(concat,contacts);
+   concat[0]=0;
+  }
+  fclose(contacts);
+  return -1;
+ }
+ int read_from_file(struct phonebook p[])
+ {
+  FILE *f1;
+  char *output,*token,line[100];
+  int j=-1,phno;
+    const char s[2]=",";
+    f1=fopen("c://tc//bin//phone.txt","r");
+    if(f1==NULL)
+     {
+      printf("failed to open file to read contacts");
+      return -1;
+     }
+    while(1)
+    {
+     output = fgets(line,100,f1);
+      if(output==NULL)
+      break;
+      j++;
+      token=strtok(line,s);
+      strcpy(p[j].name,token);
+      token=strtok(NULL,s);
+      strcpy(p[j].email,token);
+      token=strtok(NULL,s);
+      phno=atoi(token);
+      p[j].ph_no=phno;
+    }
+    fclose(f1);
+  return -1;
+ }
